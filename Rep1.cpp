d876ab3c616251c252d195c900beac37e7028b9f
PIBO0120014 Ащеулов Андрей Витальевич <PIBO0120014@sibupk.su>
2022-02-12 13:27:00 +0700

    ver1

---
+++ Rep1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int g;
+    cout << "Введите год" << endl;
+    cin >> g;
+    if (g % 4 == 0)
+    {
+        cout << "Год высокосный" << endl;
+    }
+    else
+    {
+        cout << "Год не высокосный" << endl;
+    }
+
+
+
+}
